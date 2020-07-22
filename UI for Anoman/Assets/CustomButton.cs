cc7d461e68da6d03e51dd490847e5e0bbd006f30
Teguh Ikhlas <teguh@adhivasindo.co.id>
2020-07-22 15:43:43 +0700

    basic

---
+++ UI for Anoman/Assets/CustomButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomButton : MonoBehaviour
+{
+    public GameObject imageBig;
+    public GameObject imageSmall;
+    public GameObject text;
+
+    public void Selected(){
+        SceneManager.Instance.mainImage.sprite = imageBig.GetComponent<Image>().sprite;
+        SceneManager.Instance.DisableAllButtons();
+        imageBig.SetActive(true);
+        imageSmall.SetActive(false);
+        text.SetActive(true);
+    }
+
+    public void Unselected(){
+        imageBig.SetActive(false);
+        imageSmall.SetActive(true);
+        text.SetActive(false);
+    }
+
+}
